f61e0b7cb8deb981b9c715fd60618d1826cdf5c6
icyman926 <378678435@qq.com>
2026-06-06 17:31:16 +0800

    Remove test API

--- website/pages/api/test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: 'POST works!', body: req.body });
-  } else if (req.method === 'GET') {
-    res.status(200).json({ message: 'GET works!' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
